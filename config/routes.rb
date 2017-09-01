c47e8006e5baea91139416c3b0e72ee408f01679
Ari Brenner <ari.brenner@generalassemb.ly>
2017-09-01 01:48:29 -0400

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'teachers#index'
 
   resources :teachers, only: [:index, :show] do
     resources :courses, only: [:index, :show]
