c69daa3864bd8b34bb881f06742c0b7b1f7fa97c
Ari Brenner <ari.brenner@generalassemb.ly>
2017-09-01 00:20:47 -0400

    add AR relations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+  belongs_to :teacher
+  has_many :students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ApplicationRecord
+  belongs_to :course
+  has_one :teacher, through: :course
 end
