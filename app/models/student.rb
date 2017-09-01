df6d7d5a743f720bcc7304ce302d92b70f4faddd
Ari Brenner <ari.brenner@generalassemb.ly>
2017-09-01 02:36:03 -0400

    note about `peers` method

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,6 +7,9 @@ class Student < ApplicationRecord
   def peers
     Student.where(course_id: course_id)
   end
+  # we could also define `peers` like this:
+  # has_many :peers, through: :course, source: :students
+  # this is actually prefered way. check out the docs for an explaination!
 
   def say_hi
     "Hi, I am #{name} and I am #{age} years old"
